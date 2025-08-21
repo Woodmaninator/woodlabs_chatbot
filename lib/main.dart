164ee3fff602328442b379ef8a3a19bfd0b33e99
Woodmaninator <woodmaninator@gmail.com>
2025-08-21 21:06:42 +0200

    added proper window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:window_size/window_size.dart';
 import 'package:woodlabs_chatbot/app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Woodlabs Chatbot');
+    setWindowMaxSize(const Size(1000, 600));
+    setWindowMinSize(const Size(1000, 600));
+  }
+
   runApp(ProviderScope(child: const App()));
 }
